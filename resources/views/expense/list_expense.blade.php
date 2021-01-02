2f74994dd9df9f2fa4b897f6e18c3ec336c69420
carllosmattos <30847259+carllosmattos@users.noreply.github.com>
2021-01-02 00:48:07 -0200

    modify expense(blade list)

--- resources/views/expense/list_expense.blade.php
+++ resources/views/expense/list_expense.blade.php
@@ -61,7 +61,7 @@
         <td>{{ $expense->amount}}</td>
         <td>% {{ $expense->discount}}</td>
         <td>R$ {{ $expense->amount_paid}}</td>
-        <td>{{date('d-m-Y', strtotime($solicitacao->data))}} - {{ $expense->hora}}</td>
+        <td>{{date('d-m-Y', strtotime($expense->data))}} - {{ $expense->hora}}</td>
         <td>
           <?php
             $dateNow = date('Y-m-d H:i:s'); //2020-11-26 13:45:29
