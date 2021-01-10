67032bec23e2ffd872311b5795ad90f4d307cfec
carllosmattos <30847259+carllosmattos@users.noreply.github.com>
2021-01-10 16:18:07 -0200

    paginate authorizacao 11

--- app/Http/Controllers/AuthorizacaoController.php
+++ app/Http/Controllers/AuthorizacaoController.php
@@ -56,7 +56,7 @@ class AuthorizacaoController extends Controller
   //--------------------- Listar Autorização----------------------//
   public function list_authorizacoes()
   {
-    $authorizacoes = Authorizacao::sortByDesc("id")->paginate(10);
+    $authorizacoes = Authorizacao::paginate(10)->orderby('id', 'asc');
     return view('authorizacao/list_authorizacao', compact('authorizacoes'));
   }
   //------------------------------------------------------------//
