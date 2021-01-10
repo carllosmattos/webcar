7a90155435587f23659e0bcb5557ae804eb4cccd
carllosmattos <30847259+carllosmattos@users.noreply.github.com>
2021-01-10 18:11:26 -0200

    paginate authorizacao 32

--- resources/views/authorizacao/list_authorizacao.blade.php
+++ resources/views/authorizacao/list_authorizacao.blade.php
@@ -21,7 +21,6 @@
             <div class="col-col-md-3">
               <button type="submit" class="btn btn-primary">Pesquisar</button>
             </div>
-  {{ $aut->links() }}
           </label>
         </fieldset>
       </form>
@@ -109,6 +108,8 @@
 
     </td>
   </tr>
+  
+  {{ $aut->links() }}
   @endforeach
   </tbody>
   </table>
